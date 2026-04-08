2dfdcd48785d3b48de4b45ae8faae2ed1c1c9e9c
Viktor Tsedrik <123331550+purposefulman@users.noreply.github.com>
2026-04-08 13:19:19 +0300

    Rename package from 'hashcheck' to 'hashcheck-tool'

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="hashcheck",
+    name="hashcheck-tool",
     version="1.0.0",
     author="Viktor Tsedrik",
     author_email="viktor_freelance@proton.me",
